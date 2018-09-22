5a98231c4660514a9df370b927b3a470b572471e
Raul Reyero <34958262+raul27868@users.noreply.github.com>
2018-09-22 11:24:05 +0200

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,4 +1,5 @@
 #Web Scrap https://www.danmurphys.com.au/red-wine/rose
+# json: https://gist.github.com/raul27868/a5010f230b7621950f23acb7ca87b249
 
 import json       # for manage json values: parsed_json = json.loads (json_string)
 import requests   # for http requests
@@ -20,47 +21,48 @@ data = {'pageNumber': 1,'sortType':'Relevance','pageSize': 20,'subDepartment':'r
 
 
 def f(n):
-        #Devuelve el nombre unido a un array con los otros datos de la extructura : ["Products"][0]["AdditionalDetails"])[0]["Value"]
-        #   con re.sub(r'^\s+' eliminamos con Exppresion Regular los espacios al priciopio
+        #Returns the values obtained for each array (20 per page):
+        #   First, the value is returned in the key ["Name"]. Then it returns the values with keys of the FIELDS array that are in the structure ["Products"] [0] ["AdditionalDetails"]
+        #   re.sub(r'^\s+'... we eliminated with Regular Expression the spaces at the beginning
         return [n["Name"]] + map(  lambda x : re.sub(r'^\s+', "",extract(x,n) ),FIELDS)
  
     
 def extract(r,n):
+    #We obtain the values of the key passed by parameter.        
     try:
         return  filter(lambda x : x["Name"]==r  ,  n["Products"][0]["AdditionalDetails"])[0]["Value"]   
-    
     except IndexError as error: return ""
 
 
 
 def scrap_danmurphys(page_from , page_to):
-
+    #Principal function
     data["pageNumber"] =  page_from
     
     print "page:"  ,page_from
     print "******************************************************"
     print ""
-   
-    
+
     json_data = map(f,  json.loads(requests.post('https://api.danmurphys.com.au/apis/ui/Browse', headers=headers, data=json.dumps(data) ).text)["Bundles"]   )
     
-    #print json_data
-    #print ""
-    
+    #To detect the last call to web server
     if(page_from == page_to or  json_data == []   ):
         return json_data
     else:
+        # Recursion: call this function again if there are pages for scrap
         return scrap_danmurphys(page_from+1,page_to) + json_data
-    
 
-#Escribe el resultado de la variable DATA en el fichero danmurphys.csv
+
+
+
+#Write the result in the file danmurphys.csv
 with io.open('danmurphys.csv', 'w',closefd=True) as file:
-    #Escibre las cabeceras entre comillas y separados por ;   
+    #Write the headers in quotes and separated by;   
     file.write(
                     unicode(";".join(map(lambda campo: '"'+ re.sub(r'^\s+', "",campo ) +'"' ,["Name"] + CAMPOS)+["\n"]))
               )   
                
-    #Escibre los datos entre comillas y separados por ;    
+    #Write the data in quotes and separated by;
     map(lambda fila:   file.write(
                     unicode(";".join(map(lambda campo: '"'+ re.sub(r'^\s+', "",campo ) +'"' ,fila)+["\n"]))
                                ),      
